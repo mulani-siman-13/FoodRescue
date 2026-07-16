6085770b74e7b46d59653bb168f1bbb97b1d34b0
bhagyashree21 <bhagyashreepatil2116@gmail.com>
2026-07-16 23:31:19 +0530

    Backend setup completed

--- js/script.js
+++ js/script.js
@@ -15,3 +15,21 @@ L.marker([18.5314, 73.8446]).addTo(map)
 
 L.marker([18.5091, 73.8326]).addTo(map)
 .bindPopup("<b>Cafe Goodluck</b><br>🍛 32 Meals Ready for Pickup");
+
+L.marker([18.5140,73.8397]).addTo(map)
+.bindPopup("<b>Wadeshwar FC Road</b><br>🍛 14 Meals Available");
+
+L.marker([18.5362,73.8930]).addTo(map)
+.bindPopup("<b>Barbeque Nation</b><br>🍽️ 38 Meals Available");
+
+L.marker([18.5209,73.8567]).addTo(map)
+.bindPopup("<b>McDonald's JM Road</b><br>🍔 12 Meals Available");
+
+L.marker([18.5450,73.9050]).addTo(map)
+.bindPopup("<b>Starbucks Koregaon Park</b><br>🥪 9 Meals Available");
+
+L.marker([18.5602,73.8075]).addTo(map)
+.bindPopup("<b>Burger King Hinjawadi</b><br>🍟 17 Meals Available");
+
+L.marker([18.5318,73.8477]).addTo(map)
+.bindPopup("<b>Hotel Rupali</b><br>🥘 21 Meals Available");
